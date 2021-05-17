bf9b05c4459c7af36484057b563eb4df4ab09ad4
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-05-17 13:43:48 +0500

    added comments

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -5,10 +5,12 @@ import {connect} from 'react-redux'
 import { setUserProfile } from '../../redux/profileReducer';
 
 
+//Данная компонента осуществляет работу, связанную с отправкой запроса на сервер
 class ProfileContainer extends React.Component{
 
     //все side-effects делаются в методе жизненного цикла ComponentDidMount()
     componentDidMount() {
+        debugger
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
             //ответ с сервера 
             .then(response =>{
@@ -24,5 +26,6 @@ class ProfileContainer extends React.Component{
 }
 
 let mapStateToProps = (state) => ( {profile: state.profilePage.profile} )
-//Создание контейнерной компоненты поверх ProfileContainer
+
+//Создание контейнерной компоненты поверх ProfileContainer. Данная СС делает работу, связанную с запросами к store и получае от него callbacks
 export default connect (mapStateToProps, {setUserProfile}) (ProfileContainer);
