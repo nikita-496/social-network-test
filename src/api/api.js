c4907a3388a1107331decdfbad16bb4be132dd51
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-05-19 10:15:18 +0500

    refactoring: creating an api object

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,26 @@
-import axios from 'axios'
+import  * as axios from 'axios'
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+    "API-KEY" : "d2826521-0085-4ffb-9d35-d4ec51f7d121"
+    }
+})
 
  //запрашиваем пользователей
-export const getUsers = (currentPage, pageSize) => {
-   return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
-                withCredentials: true
-            })
+export const usersAPI = {
+    getUsers (currentPage = 1, pageSize = 100) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => {
                 return response.data
             });
+        },
+    follow (userId) {
+        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow${userId}`)
+    },
+    unfollow (userId) {
+        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow${userId}`)
+    },
 }
 
