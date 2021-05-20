ae62cf0037966a41b86f92ed5d66a7c5c4dd50f4
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-05-20 11:41:48 +0500

    componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -27,7 +27,18 @@ class ProfileStatus extends React.Component {
         }) 
      }
 
+     componentDidUpdate(prevProps, prevState) {
+         
+         if(prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+         }
+        
+         console.log("componentDidUpdate")
+     }
    render() {
+    console.log("render")
     return (
         <div>
             {!this.state.editMode ?
